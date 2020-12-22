69b5f03d9fca8572061b7d7c9d0a309784ab11b4
Gabriel Maximo <gbielmaximo@gmail.com>
2020-12-22 23:10:26 +0000

    se mexe!

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
-    type = "canvas"
-}
-
-PIXI.utils.sayHello(type)
+//Aliases
+let Application = PIXI.Application,
+    Container = PIXI.Container,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite,
+    Rectangle = PIXI.Rectangle;
 
 //Create a Pixi Application
-let app = new PIXI.Application({width: 512, height: 512});
+let app = new Application({ 
+    width: 256, 
+    height: 256,                       
+    antialias: true, 
+    transparent: false, 
+    resolution: 1
+  }
+);
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+loader
+  .add("/sprites/cubo.png")
+  .load(setup);
+
+//Define any variables that are used in more than one function
+let cat;
+
+function setup() {
+
+  //Create the `cat` sprite 
+  cat = new Sprite(resources["/sprites/cubo.png"].texture);
+  cat.y = 96; 
+  app.stage.addChild(cat);
+ 
+  //Start the game loop 
+  app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta){
+
+  //Move the cat 1 pixel 
+  cat.x += 1;
+  if (cat.x > app.width) {
+      cat.x = -32
+  }
+  //Optionally use the `delta` value
+  //cat.x += 1 + delta;
+}
